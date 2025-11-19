cef6b3588dd3b3a3f3335b483feb131592e6423e
Paul Bichl <paul.bichl@edu.fh-joanneum.at>
2025-11-19 11:26:21 +0100

    removed ncnn part from setup.py

--- src/roxy/setup.py
+++ src/roxy/setup.py
@@ -1,7 +1,6 @@
 import os
 
 import gdown
-from ultralytics import YOLO
 
 # download nano classification model
 file_id = "1XBHIwLef2uT6u4mAxOi03UeHbfI8uEOU"
@@ -9,8 +8,3 @@ url = f"https://drive.google.com/uc?id={file_id}"
 os.makedirs("./tmp/models", exist_ok=True)
 output = "./tmp/models/model.pt"
 gdown.download(url, output, quiet=False)
-
-
-# model is stored as ./tmp/models/model.pt, convert to ncnn format => better performance on raspberry pi
-model = YOLO("./tmp/models/model.pt")
-model.export(format="ncnn")
