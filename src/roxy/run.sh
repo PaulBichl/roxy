76430e29c6d57f79839cb2c77f30b6fafc179e10
Paul Bichl <paul.bichl@edu.fh-joanneum.at>
2025-11-20 11:00:18 +0100

    changed run.sh

--- src/roxy/run.sh
+++ src/roxy/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Stop and remove any old container with this name
 echo "Stopping and removing old container..."
 docker stop motion-detector
 docker rm motion-detector
 
-# Run the new, correct container in the background
 echo "Starting container..."
 docker run -d \
   --name motion-detector \
   --restart unless-stopped \
   --privileged \
+  --device /dev/gpiomem \
+  --device /dev/gpiochip0 \
+  --device /dev/gpiochip1 \
   -v /run/udev:/run/udev:ro \
   -v "$PWD/data.json:/camera/data.json:ro" \
   p5-camera
