07fdb0b765174806cbf927fdde5e6799e7d0e93d
Isabel <isabel.e.thorsen@gmail.com>
2026-04-10 22:04:10 +0200

    Update file_id for model download in setup.py to correct the model reference

--- src/roxy/setup/setup.py
+++ src/roxy/setup/setup.py
@@ -4,7 +4,7 @@ import gdown
 from ultralytics import YOLO
 
 # download nano classification model in .pt format
-file_id = "18KgHfnAvAmR7DuYrtCHeY7vpTvVb4oVr"
+file_id = "1-jPnj9iWWz7V60TIRJ7TSHhhF6dNQ_c2"
 url = f"https://drive.google.com/uc?id={file_id}"
 os.makedirs("./tmp/models", exist_ok=True)
 output = "./tmp/models/model.pt"
